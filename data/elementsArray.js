16b9dc63d9703cd4cabf50408de0bbcf8d1ca06f
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2022-09-29 21:30:50 -0400

    Add files via upload

---
+++ data/elementsArray.js
@@ -0,0 +1,59 @@
+// Dedicated to God the Father
+// All Rights Reserved Christopher Topalian Copyright 2000-2022
+// https://github.com/ChristopherTopalian
+// https://github.com/ChristopherTopalian/Topalian_Data_Navigator
+// elementsArray.js
+
+/*
+{
+    name: " ",
+    url: " ",
+    description: " ",
+    urlSource: " "
+},
+*/
+
+let elementsArray = [
+{
+    name: "Hydrogen",
+    // url: "https://collegeofscripting.files.wordpress.com/2022/09/hydrogen-wikipedia.pdf",
+    url: "pdf/Hydrogen-Wikipedia.pdf",
+    description: "<b>Hydrogen</b> is the chemical element with the symbol <b>H</b> and <b>atomic number 1</b>. Hydrogen is the lightest element. At standard conditions hydrogen is a gas of diatomic molecules having the formula H2. It is colorless, odorless, tasteless,[8] non-toxic, and highly combustible. Hydrogen is the most abundant chemical substance in the universe, constituting roughly 75% of all normal matter.",
+    urlSource: "https://en.wikipedia.org/wiki/Hydrogen"
+},
+
+{
+    name: "Helium",
+    // url: "https://collegeofscripting.files.wordpress.com/2022/09/helium-wikipedia.pdf",
+    url: "pdf/Helium-Wikipedia.pdf",
+    description: "<b>Helium</b> is a chemical element with the symbol <b>He</b> and <b>atomic number 2</b>. It is a colorless, odorless, tasteless, non-toxic, inert, monatomic gas and the first in the noble gas group in the periodic table.[a] Its boiling and melting point are the lowest among all the elements. It is the second lightest and second most abundant element in the observable universe (hydrogen is the lightest and most abundant). It is present at about 24% of the total elemental mass, which is more than 12 times the mass of all the heavier elements combined.",
+    urlSource: "https://en.wikipedia.org/wiki/Helium"
+},
+
+{
+    name: "Lithium",
+    // url: "https://collegeofscripting.files.wordpress.com/2022/09/lithium-wikipedia.pdf",
+    url: "pdf/Lithium-Wikipedia.pdf",
+    description: "<b>Lithium</b> is a chemical element with the symbol <b>Li</b> and <b>atomic number 3</b>. It is a soft, silvery-white alkali metal. Under standard conditions, it is the least dense metal and the least dense solid element. Like all alkali metals, lithium is highly reactive and flammable, and must be stored in vacuum, inert atmosphere, or inert liquid such as purified kerosene or mineral oil. When cut, it exhibits a metallic luster, but moist air corrodes it quickly to a dull silvery gray, then black tarnish. It never occurs freely in nature, but only in (usually ionic) compounds, such as pegmatitic minerals, which were once the main source of lithium.",
+    urlSource: "https://en.wikipedia.org/wiki/Lithium"
+},
+
+{
+    name: "Electrolysis",
+    // url: "https://collegeofscripting.files.wordpress.com/2022/09/electrolysis-wikipedia.pdf",
+    // url: "https://collegeofscripting.weebly.com/electrolysis.html",
+    url: "page/electrolysis.html",
+    description: "In chemistry and manufacturing, <b>electrolysis</b> is a technique that uses direct electric current (DC) to drive an otherwise non-spontaneous chemical reaction. Electrolysis is commercially important as a stage in the separation of elements from naturally occurring sources such as ores using an electrolytic cell. The voltage that is needed for electrolysis to occur is called the decomposition potential. The word \"lysis\" means to separate or break, so in terms, electrolysis would mean \"breakdown via electricity\".",
+    urlSource: "https://en.wikipedia.org/wiki/Electrolysis"
+}
+
+];
+
+/*
+{
+    name: " ",
+    url: " ",
+    description: " ",
+    urlSource: " "
+},
+*/
